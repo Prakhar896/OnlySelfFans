3f9c17a4af07a51e30b4586eb29af8a98fedf2cb
Prakhar Trivedi <53103894+Prakhar896@users.noreply.github.com>
2023-06-25 11:20:41 +0800

    Initial Commit

---
+++ OnlySelfFans/OnlySelfFansApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnlySelfFansApp.swift
+//  OnlySelfFans
+//
+//  Created by Prakhar Trivedi on 25/6/23.
+//
+
+import SwiftUI
+
+@main
+struct OnlySelfFansApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
